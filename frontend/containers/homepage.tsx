54146eaec421b40ec09167fafa91d899674ed7d2
David Nienge <davidnienge@gmail.com>
2025-10-12 17:06:38 +0100

    property search

--- frontend/containers/homepage.tsx
+++ frontend/containers/homepage.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/hero";
 import Navbar from "@/components/shared/navbar";
+import PropertyTypes from "@/components/property-types";
 
 export default function Homepage() {
   return (
     <div className="w-full">
       <Navbar />
       <Hero />
+        <PropertyTypes />
     </div>
   );
 }
